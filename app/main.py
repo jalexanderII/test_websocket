1fff1a7847f174e4694b7ae0ca4cfe3e288fc291
Joel Alexander <a.joeljr@hotmail.com>
2025-01-27 18:34:29 -0500

    refactor

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from presentation.api.http_endpoints import router as http_router
-from presentation.websocket.chat_websocket import router as websocket_router
-from infrastructure.db.database import Base, engine
-from config import HOST, PORT
+
+from app.db.database import Base, engine
+from app.config.env import HOST, PORT
+from app.api.routes.http_endpoints import router as http_router
+from app.api.routes.chat_websocket import router as websocket_router
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
